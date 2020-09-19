45ecee5e5055151f2de1631361d6dda0f7fc24a7
Deepika-92 <71193396+Deepika-92@users.noreply.github.com>
2020-09-18 20:36:46 -0400

    part-one-answer

---
+++ script.js
@@ -0,0 +1,12 @@
+function add () {
+    
+    var x = parseInt(document.getElementById("varX").value);
+    var y = parseInt(document.getElementById("varY").value);
+  
+    var result = x + y;
+  
+    
+    document.getElementById("varResult").value = Result;
+    alert('Hi ' + name + '!')
+    
+}
